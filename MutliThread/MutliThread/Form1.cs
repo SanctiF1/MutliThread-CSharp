44726ffa957ca26e554199deb9776e8d836ff894
SanctiF1 <vctoan@gmail.com>
2015-03-11 15:19:24 +0700

    Multi thread

---
+++ MutliThread/MutliThread/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace MutliThread
+{
+    public partial class Form1 : Form
+    {
+        Thread t1, t2;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            label2.Text = DateTime.Now.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        static void ThreadJob()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Other thread: " + i.ToString (), i);
+                Thread.Sleep(1000);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ThreadStart job = new ThreadStart(ThreadJob);
+            Thread thread = new Thread(job);
+            thread.Start();
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    label1.Text = "Main thread: " + i.ToString();
+            //    Thread.Sleep(2000);
+            //}
+
+            Thread main = new Thread(ThreadMain);
+            main.Start();
+
+
+            
+
+            
+            
+           
+        }
+
+        private void ThreadMain()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Main thread: " + i.ToString(), i);
+                Thread.Sleep(2000);
+                ShowThread("thread " + i.ToString());
+            }
+        }
+
+        private void ThreadForm()
+        {
+            Form2 frm = new Form2();
+            frm.Show();
+            Application.Run();
+        }
+        private void ShowThread(string s)
+        {
+            if (label1.InvokeRequired)
+            {
+                label1.BeginInvoke(new MethodInvoker(delegate() { ShowThread(s); }));
+            }
+            else
+            {
+                lock (label1)
+                {
+                    label1.Text = s;                    
+                }
+            }
+        }
+
+
+
+        private void AppendText(string s)
+        {
+            if (textBox1.InvokeRequired)
+            {
+                textBox1.BeginInvoke(new MethodInvoker(delegate() { AppendText(s); }));
+            }
+            else
+            {
+                lock (textBox1)
+                { 
+                    textBox1.AppendText(s);
+                    textBox1.AppendText(Environment.NewLine);
+                }
+            }
+        }
+
+        private void Thread1()
+        {
+            while (true)
+            {
+                AppendText("This is thread 1");
+                Thread.Sleep(500);
+            }
+        }
+
+        private void Thread2()
+        {
+            while (true)
+            {
+                AppendText("This is thread 2");
+                Thread.Sleep(500);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            t1 = new Thread(Thread1);
+            t2 = new Thread(Thread2);
+
+            t1.Start();
+            t2.Start();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label2.Text = DateTime.Now.ToString();
+            Thread thrform = new Thread(ThreadForm);
+            thrform.Start();
+        }
+
+    }
+}
---
+++ MutliThread/MutliThread/Form2.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Threading;
+
+namespace MutliThread
+{
+    public partial class Form2 : Form
+    {
+        private string sCon;
+        private bool chkRun;
+        private DataTable dt;
+        private Thread t;
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            
+            
+        }
+
+        private void btnStatus_Click(object sender, EventArgs e)
+        {
+            if (t == null || (t.ThreadState != ThreadState.Running && t.ThreadState != ThreadState.WaitSleepJoin ))
+            {
+                t = new Thread(getInfo);
+                t.Start();
+                btnStatus.Text = "Stop";
+            }
+            else
+            {                
+                t.Abort();
+                btnStatus.Text = "Start";
+            }
+           
+            
+        }
+
+        private void getInfo()
+        {
+            while (true)
+            {
+                Thread.Sleep(5000);
+                    dt = new DataTable();
+                    sCon = "Server= 10.0.10.21;Database=POSCOMMON;User Id=sa;Password=pwd#123;";
+                    SqlConnection con;
+                    SqlCommand command;
+                    SqlDataAdapter sqlDataAdapter = new SqlDataAdapter();
+
+                    con = new SqlConnection(sCon);
+                    con.Open();
+                    command = new SqlCommand(" select ROW_NUMBER() over(order by createdDate desc) as stt,  CreatedDate, id,DocEntry, Transtype, ShopCode, Docdate, Remarks,Company,  Messages, IsSuccess from POSSyncSaleLog order by createdDate desc ", con);
+                    command.CommandType = CommandType.Text;
+                    sqlDataAdapter.SelectCommand = command;
+                    sqlDataAdapter.Fill(dt);
+                    con.Close();
+                    displayData(dt);
+            };
+            //dataGridView1.DataSource = dt;
+            //dataGridView1.Refresh();
+        }
+
+        private void displayData(DataTable s)
+        {
+            if (dataGridView1 .InvokeRequired)
+            {
+                dataGridView1.BeginInvoke(new MethodInvoker(delegate() { displayData(s); }));
+            }
+            else
+            {
+                lock (dataGridView1)
+                {
+                    dataGridView1.DataSource = s;
+                    dataGridView1.Refresh();
+                }
+            }
+        }
+        
+    }
+}
